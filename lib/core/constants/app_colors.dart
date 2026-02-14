0720f9d5206ab9c621e0e79b4ba702caed0d8537
Mooaz Mohammed <madhmhmdsyd110@gmail.com>
2026-02-14 19:42:52 +0200

    pick colors and add images assets to pubspec

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -1,4 +1,8 @@
-class AppColors {
-  
+import 'package:flutter/widgets.dart';
 
+class AppColors {
+  static const Color goldColor = Color(0xffeec430);
+  static const Color silverColor = Color(0xffA9A9A9);
 }
+
+
---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,4 @@
+class AppImages {
+  static const String goldImage = "assets/goldImage.png";
+  static const String silverImage = "assets/silver.jfif";
+}
